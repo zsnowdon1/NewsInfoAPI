c98698ec257beda69bbf00e4c2da2c1f2c6efaa2
zlsnowdo <zlsnowdo@ncsu.edu>
2022-07-06 19:52:00 -0500

    Moved local project to github

---
+++ NewsInfoAPI/Controllers/NewsInfoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewsInfoAPI.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NewsInfoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NewsInfoController : ControllerBase
+    {
+
+        private readonly ILogger<NewsInfoController> _logger;
+
+        public NewsInfoController(ILogger<NewsInfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetBasketball()
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load("https://www.espn.com/nba/");
+
+            Debug.WriteLine("Done");
+            foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//section[@class='" + "contentItem contentItem--collection" + "']"))
+            {
+                NewsItem news = new NewsItem();
+                //contentItem__content contentItem__content--story has - image has - video contentItem__content--collection
+                Debug.WriteLine(node.ChildNodes[1].ParentNode.InnerText);
+
+                //news.Title = node.ChildNodes[0].ChildNodes[0].InnerText;
+                //Debug.WriteLine(node.ChildNodes[0].InnerText);
+                //Debug.WriteLine(node.ChildNodes[1].InnerText);
+                //Debug.WriteLine(node.ChildNodes[1].ChildNodes[0].InnerText);
+                Debug.WriteLine(' ');
+            }
+                return null;
+            //return Ok(htmlDoc.DocumentNode.SelectSingleNode("//head//contentItem__title contentItem__title--video"));
+        }
+    }
+}
---
+++ NewsInfoAPI/Models/NewsItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace NewsInfoAPI.Models
+{
+    public class NewsItem
+    {
+
+        public string Title { get; set; }
+
+        public string Subtitle { get; set; }
+
+        public string PicUrl { get; set; }
+
+        public NewsItem()
+        {
+
+        }
+    }
+}
