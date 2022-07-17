8e0bee0f73f45f97e7309deda4d6851e821e5331
zlsnowdo <zlsnowdo@ncsu.edu>
2022-07-16 23:45:15 -0500

    Found image url in html

--- NewsInfoAPI/Controllers/NewsInfoController.cs
+++ NewsInfoAPI/Controllers/NewsInfoController.cs
@@ -31,18 +31,17 @@ namespace NewsInfoAPI.Controllers
             HtmlWeb web = new HtmlWeb();
             var htmlDoc = web.Load("https://www.espn.com/nba/");
             List<NewsItem> items = new List<NewsItem>();
-            foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//section[@class='"
-                + "contentItem__content contentItem__content--story has-image has-video" +
-                " contentItem__content--collection contentItem__content--enhanced contentItem__content--fullWidth" + "']"))
+            foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//section[@class='" + "contentItem contentItem--collection" + "']"))
             {
-                Debug.WriteLine(node.SelectSingleNode("//img").Attributes["data-default-src"].Value);
 
-                //items.Add(new NewsItem
-                //{
-                //    Title = node.SelectSingleNode("//h1[@class='" + "contentItem__title contentItem__title--story" + "']").InnerText,
-                //    Subtitle = node.SelectSingleNode("//p[@class='" + "contentItem__subhead contentItem__subhead--story" + "']").InnerText,
-                //    PicUrl = node.SelectSingleNode("//img[@class='media-wrapper_image  imageLoaded lazyloaded']").Attributes["data-default-src"].Value
-                //});
+                HtmlNode urlNode = node.SelectSingleNode("//picture[@class='" + "media-wrapper_image" + "']");
+
+                items.Add(new NewsItem
+                {
+                    Title = node.SelectSingleNode("//h1[@class='" + "contentItem__title contentItem__title--story" + "']").InnerText,
+                    Subtitle = node.SelectSingleNode("//p[@class='" + "contentItem__subhead contentItem__subhead--story" + "']").InnerText,
+                    PicUrl = urlNode.ChildNodes[2].Attributes[0].Value
+                });
             }
             for(int i = 0; i < items.Count; i++)
             {
